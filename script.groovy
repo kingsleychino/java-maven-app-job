71686d9c7b293b73d75661b09e99d640ec4648e4
kingsleychino <kingsleychinono@gmail.com>
2026-01-11 02:47:17 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,11 @@ def buildJar() {
 
 def buildImage() {
   echo 'building the application...'
-  sh 'mvn package'
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    sh 'docker build -t kingsleychino/demo-app:jma-2.0 .'
+    sh 'echo $PASS | docker login -u $USER --password-stdin'
+    sh 'docker push kingsleychino/demo-app:jma-2.0'
+  }
 }
 
 def deployApp() {
